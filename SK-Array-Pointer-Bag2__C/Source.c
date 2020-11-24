5474e11a312612d578ea769b95a5356223b78cf5
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2020-11-24 18:15:01 +0700

    Add project files.

---
+++ SK-Array-Pointer-Bag2__C/Source.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	int array[] = { 11, 13, 17, 19 };
+	int x;
+	int* aptr;
+
+	aptr = array;		/* no & needed for an array */
+
+	for (x = 0; x < 4; x++)
+	{
+		printf("Element %d: %d\n", x + 1, *aptr);
+		aptr++;
+	}
+
+	_getch();
+	return(0);
+}
